d4663773e1bf5840e165bb7838b249c7cb3a1f70
Jachdich <jamesedkitching@gmail.com>
2021-05-18 22:06:10 +0100

    variables, probably wrong tbh but it kinda works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Write;
 use std::env;
 extern crate dirs;
+use std::collections::HashMap;
 
 use std::path::{Path, PathBuf};
 
@@ -28,44 +29,13 @@ struct Shell {
     w_dir: PathBuf,
     rc_path: PathBuf,
     hist_path: PathBuf,
-
-}
-
-fn parse_argv(total: String) -> Vec<String> {
-    let mut res: Vec<String> = Vec::new();
-    let mut pos: usize = 0;
-    let data: Vec<char> = total.chars().into_iter().collect();
-    res.push("".to_string());
-    while pos < data.len() {
-        match data[pos] {
-            ' ' => {
-                res.push("".to_string());
-                pos += 1;
-            }
-            '$' => {
-                
-            }
-            '"' /*"*/ => {
-                pos += 1;
-                while pos < data.len() && data[pos] != '"' /*"*/ {
-                    res.last_mut().unwrap().push(data[pos]);
-                    pos += 1;
-                }
-                pos += 1;
-            }
-            '\n' => pos += 1,
-            c => {
-                res.last_mut().unwrap().push(c);
-                pos += 1;
-            }
-        }
-    }
-    res
+    vars: HashMap<String, String>,
 }
 
 impl Shell {
     fn new() -> Self {
-        env::set_var("PS1", "");
+        let mut vars: HashMap<String, String> = HashMap::new();
+        vars.insert("PS1".to_string(), ">".to_string());
         let w_dir = std::env::current_dir().unwrap();
         let mut rc_path = dirs::home_dir().unwrap();
         let mut hist_path = dirs::home_dir().unwrap();
@@ -73,11 +43,12 @@ impl Shell {
         hist_path.push(".josh_history");
         Shell {
             w_dir, rc_path, hist_path,
+            vars,
         }
     }
     
     fn get_ps1(&self) -> String {
-        format!("{}", env::var("PS1").unwrap())
+        format!("{}", self.vars.get("PS1").unwrap())
     }
 
     fn execute_command(&mut self, command: &str, argv: &[String]) -> bool {
@@ -136,12 +107,107 @@ impl Shell {
                 println!();
                 return;
             }
+            if input.ends_with('\n') {
+                input.pop();
+                if input.ends_with('\r') {
+                    input.pop();
+                }
+            }
 
-            let argv = parse_argv(input);
-            if !self.execute_command(&argv[0], &argv[1..]) {
-                break;
+            let argv = self.parse_argv(input);
+            match argv {
+                Some(argv) => {
+                    if argv.len() == 0 { continue; }
+                    if !self.execute_command(&argv[0], &argv[1..]) {
+                        break;
+                    }
+                }
+                None => {
+                }
+            }
+            
+        }
+    }
+
+    fn eval_vars(&mut self, data: Vec<String>) -> Vec<String> {
+        let assign_regex = regex::Regex::new(r#"^\w+=.+"#).unwrap();
+        let mut res: Vec<String> = Vec::new();
+        for arg in data {
+            if assign_regex.is_match(&arg) {
+                let (name, value) = &arg.split_once('=').unwrap();
+                self.vars.insert(name.to_string(), value.to_string());
+            } else {
+                res.push(arg);
             }
         }
+        res
+    }
+
+    fn split_with_strings(&mut self, data: Vec<char>) -> std::option::Option<Vec<String>> {
+        let mut res: Vec<String> = Vec::new();
+        let mut pos: usize = 0;
+        res.push("".to_string());
+        while pos < data.len() {
+            match data[pos] {
+                ' ' => {
+                    res.push("".to_string());
+                    while pos < data.len() && data[pos] == ' ' {
+                        pos += 1;
+                    }
+                }
+                '"' /*"*/ => {
+                    pos += 1;
+                    while pos < data.len() && data[pos] != '"' /*"*/ {
+                        res.last_mut().unwrap().push(data[pos]);
+                        pos += 1;
+                    }
+                    if pos == data.len() {
+                        println!("josh: EOF while scanning string literal");
+                        return None;
+                    }
+                    pos += 1;
+                }
+                c => {
+                    res.last_mut().unwrap().push(c);
+                    pos += 1;
+                }
+            }
+        }
+        if res.last().unwrap() == "" {
+            res.pop();
+        }
+        Some(self.eval_vars(res))
+    }
+    
+    fn parse_argv(&mut self, total: String) -> std::option::Option<Vec<String>> {
+        let mut res: Vec<char> = Vec::new();
+        let mut pos: usize = 0;
+        let data: Vec<char> = total.chars().into_iter().collect();
+        while pos < data.len() {
+            match data[pos] {
+                '$' => {
+                    pos += 1;
+                    let mut name = String::new();
+                    while pos < data.len() && data[pos] != ' ' {
+                        name.push(data[pos]);
+                        pos += 1;
+                    }
+                    if self.vars.contains_key(&name) {
+                        for c in self.vars.get(&name).unwrap().chars() {
+                            res.push(c);
+                        }
+                    } else {
+                        //do nothing so far
+                    }
+                }
+                '\n' => pos += 1,
+                c => {
+                    res.push(c);
+                    pos += 1;
+                }
+            }
+        }
+        self.split_with_strings(res)
     }
 }
 
